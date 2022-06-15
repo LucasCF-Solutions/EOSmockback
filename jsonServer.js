20d4f5ea39ee9a58be32461b4a8f333a4cfddf05
Jully Anne <jara.jully.2016@gmail.com>
2022-06-15 09:33:46 -0300

    first commit

--- jsonServer.js
+++ jsonServer.js
@@ -1,19 +1,29 @@
 // server.js
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(3100,() =>{
-  console.log("public on port: 3100")
-})
+
+
 
 const path = require('path')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const routerMenu = jsonServer.router(__dirname)
+const routerDashboard = jsonServer.router(path.join(__dirname, 'dashboard.json'))
+const routerFurnaceHistoricalAlarms = jsonServer.router(path.join(__dirname, 'furnaceHistoricalAlarms.json'))
+const routerFurnaceHistoricalAnalysis = jsonServer.router(path.join(__dirname, 'furnaceHistoricalAnalysis.json'))
+const routerFurnaceRealTimeAnalysis = jsonServer.router(path.join(__dirname, 'furnaceRealTime.json'))
+const routerResourceHistoricalAnalysis = jsonServer.router(path.join(__dirname, 'resourceHistoricalAnalysis.json'))
+const routerResourceRealTimeAnalysis = jsonServer.router(path.join(__dirname, 'resourceRealTimeAnalysis.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(router)
+server.use(routerMenu)
+server.use(routerDashboard)
+server.use(routerFurnaceHistoricalAlarms)
+server.use(routerFurnaceHistoricalAnalysis)
+server.use(routerFurnaceRealTimeAnalysis)
+server.use(routerResourceHistoricalAnalysis)
+server.use(routerResourceRealTimeAnalysis)
+
+
 server.listen(3000, () => {
   console.log('JSON Server is running: 3000')
 })
